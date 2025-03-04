fad8b3919f248379f5506acb35323d8244733e29
phelan <phelan.minhtanly@gmail.com>
2025-03-04 13:33:32 +0700

    fix build

--- src/pages/profile/model.tsx
+++ src/pages/profile/model.tsx
@@ -1,27 +1,7 @@
 import View from './view';
 
 function Model() {
-
-	const handleScrollHome = () => {
-		const elementAbout = document.querySelector('#about');
-		const elementPersonal = document.querySelector('#personal');
-		if (elementAbout && elementPersonal) {
-			elementAbout?.classList.remove('-translate-x-full');
-			elementPersonal?.classList.add('translate-x-full');
-			elementPersonal?.classList.add('translate-x-full');
-		}
-	};
-	const handleScrollWork = () => {
-		const elementPersonal = document.querySelector('#personal');
-		const elementWork = document.querySelector('#work');
-		if (elementWork && elementPersonal) {
-			elementPersonal?.classList.remove('-translate-y-full');
-			elementPersonal?.classList.add('-translate-y-[200%]');
-			elementWork?.classList.add('-translate-y-[200%]');
-		}
-	};
-
-	return <View onScrollToHome={handleScrollHome} onScrollToWork={handleScrollWork} />;
+	return <View />;
 }
 
 export default Model;
